ec85709ac7449b37662f84d64304e8e3ad2f1fdd
Kalmovic <joseph.ext@rallyrd.com>
2024-01-27 01:37:02 -0300

    feat: rwact query fetching data from database

---
+++ src/queries/useGetPreviousRulings.ts
@@ -0,0 +1,42 @@
+import { UseQueryOptions, useQuery } from "@tanstack/react-query";
+import { database } from "@/firebase";
+import { ref, onValue } from "firebase/database";
+
+type PreviousRullingType = {
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  picture: string;
+  lastUpdated: string;
+  votes: {
+    negative: number;
+    positive: number;
+  };
+}[];
+
+const fetchFromFirebase = async () => {
+  return new Promise((resolve, reject) => {
+    const dbRef = ref(database, "data");
+    onValue(
+      dbRef,
+      (snapshot) => {
+        resolve(snapshot.val());
+      },
+      (error) => {
+        reject(error);
+      }
+    );
+  }) as Promise<PreviousRullingType>;
+};
+
+export const useGetPreviousRullings = (
+  options?: Partial<UseQueryOptions<PreviousRullingType>>
+) => {
+  return useQuery({
+    staleTime: 1000 * 60 * 60 * 24,
+    queryKey: ["previousRullings"],
+    queryFn: fetchFromFirebase,
+    ...options,
+  });
+};
