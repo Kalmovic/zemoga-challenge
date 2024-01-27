5c8846f78388dab18071e0c8ad4139dfef1c5a88
Kalmovic <joseph.ext@rallyrd.com>
2024-01-27 01:36:07 -0300

    feat: app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
-import { useState } from "react";
-// import "./App.css";
+import { IconButton, Button, Box, Text, Flex } from "@radix-ui/themes";
+import Card from "@/components/molecules/card";
+import { useGetPreviousRullings } from "./queries/useGetPreviousRulings";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  const { data: previousRullings, isLoading } = useGetPreviousRullings({
+    staleTime: 1000 * 60,
+  });
+
+  return (
+    <Flex direction="column" gap="3">
+      <Text
+        size="5"
+        weight="light"
+        style={{
+          color: "var(--color-dark-gray)",
+        }}
+      >
+        Previous Rullings
+      </Text>
+      {isLoading && <Text>Loading...</Text>}
+      <Flex
+        gap="3"
+        style={{
+          overflowX: "scroll",
+          width: "100%",
+        }}
+      >
+        {previousRullings?.map((rulling) => (
+          <Card key={rulling.id} {...rulling} />
+        ))}
+      </Flex>
+    </Flex>
+  );
 }
 
 export default App;
