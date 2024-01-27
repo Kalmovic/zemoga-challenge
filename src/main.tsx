2b2b143e3ff92af4664470eda6e7452338f58678
Kalmovic <joseph.ext@rallyrd.com>
2024-01-27 01:30:53 -0300

    feat: main app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-console.log(document.getElementById("root")!);
+export const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Theme radius="none" accentColor="plum">
+        <App />
+      </Theme>
+    </QueryClientProvider>
   </React.StrictMode>
 );
