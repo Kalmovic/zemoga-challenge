49efca8de12e54cc10e3f79f43333ac28883b1b1
Kalmovic <joseph.ext@rallyrd.com>
2024-01-28 01:41:52 -0300

    feat: import from cards the voting cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,7 @@
-import { IconButton, Button, Box, Text, Flex } from "@radix-ui/themes";
-import Card from "@/components/molecules/card";
-import { useGetPreviousRullings } from "./queries/useGetPreviousRulings";
+import { Cards } from "@/components/environments/cards";
 
 function App() {
-  const { data: previousRullings, isLoading } = useGetPreviousRullings({
-    staleTime: 1000 * 60,
-  });
-
-  return (
-    <Flex direction="column" gap="3">
-      <Text
-        size="5"
-        weight="light"
-        style={{
-          color: "var(--color-dark-gray)",
-        }}
-      >
-        Previous Rullings
-      </Text>
-      {isLoading && <Text>Loading...</Text>}
-      <Flex
-        gap="3"
-        style={{
-          overflowX: "scroll",
-          width: "100%",
-        }}
-      >
-        {previousRullings?.map((rulling) => (
-          <Card key={rulling.id} {...rulling} />
-        ))}
-      </Flex>
-    </Flex>
-  );
+  return <Cards />;
 }
 
 export default App;
---
+++ src/components/environments/Cards.tsx
@@ -0,0 +1,69 @@
+import { useMediaQuery, useReadLocalStorage } from "usehooks-ts";
+import { Text, Flex } from "@radix-ui/themes";
+import styled from "styled-components";
+import Card from "@/components/molecules/card";
+import { ViewEnum, ViewSelector } from "@/components/molecules/ViewSelector";
+import { useGetPreviousRullings } from "@/queries/useGetPreviousRulings";
+
+export function Cards() {
+  const view = useReadLocalStorage("view") as ViewEnum;
+  const { data: previousRullings, isLoading } = useGetPreviousRullings({
+    staleTime: 1000 * 60,
+  });
+  const isMobile = useMediaQuery("(max-width: 767px)");
+
+  return (
+    <Flex direction="column" gap="3">
+      <Flex justify="between" align="center">
+        <Text
+          size="5"
+          weight="light"
+          style={{
+            color: "var(--color-dark-gray)",
+          }}
+        >
+          Previous Rullings
+        </Text>
+        {!isMobile && !isLoading && <ViewSelector />}
+      </Flex>
+      {isLoading && <Text>Loading...</Text>}
+      {isMobile && (
+        <Flex
+          gap="3"
+          style={{
+            minWidth: "100%",
+            overflowX: "scroll",
+          }}
+        >
+          {previousRullings?.map((rulling) => (
+            <Card key={rulling.id} {...rulling} mode="grid" />
+          ))}
+        </Flex>
+      )}
+      {!isMobile && (
+        <CardsGrid mode={view}>
+          {previousRullings?.map((rulling) => (
+            <Card key={rulling.id} {...rulling} mode={view} />
+          ))}
+        </CardsGrid>
+      )}
+    </Flex>
+  );
+}
+
+const CardsGrid = styled.div<{
+  mode: "list" | "grid";
+}>`
+  display: grid;
+  grid-template-columns: ${({ mode }) =>
+    mode === "list" ? "repeat(1, 1fr);" : "repeat(3, 1fr)"};
+  gap: 21px;
+  @media (max-width: 1099px) and (min-width: 768px) {
+    grid-template-columns: ${({ mode }) =>
+      mode === "list" ? "repeat(1, 1fr);" : "repeat(2, 1fr)"};
+  }
+  @media (max-width: 767px) {
+    height: max-content;
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
